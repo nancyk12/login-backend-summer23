7ca273d9364afd92e13b4d27ed8fc5ec004b77e7
nancyk12 <nancyk12@hotmail.com>
2023-06-28 19:29:36 -0400

    added password hash compare, login

--- routes/users/controller/usersController.js
+++ routes/users/controller/usersController.js
@@ -1,23 +1,29 @@
 const User = require('../model/User')
-const { createUser, hashPassword } = require('./usersHelper')
+const { createUser, hashPassword, comparePasswords } = require('./usersHelper')
 
 module.exports = {
     login: async (req, res) => {
         try {
-          console.log(req.body);
-            if (req.body.username !== 'Brad' && req.body.username !== 'Janet' ){
-                throw {
+            // console.log(req.body);
+            
+            //check if user exists / get the user form the db
+            let foundUser = await User.findOne({username: req.body.username})
+            if (!foundUser) {
+                 throw {
                     status: 404,
                     message: "User Not Found"
                 }
-            }
+            }          
 
-            if (req.body.password !== 'abc'){
+            // check if password matches
+            let checkedPassword = await comparePasswords(req.body.password, foundUser.password)
+            if (!checkedPassword) {
                 throw {
-                    status: 403,
+                    status: 401,
                     message: "Invalid Password"
                 }
             }
+   
             res.status(200).json({
                 username: req.body.username,
                 password: req.body.password,
